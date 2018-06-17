0586e2eb11abd3dedc2bf6a30bccf7fa08c17b03
Alex Karadimos <karadalex@gmail.com>
2018-06-17 22:21:20 +0300

    changed color to match logo's

--- ipfs/web/src/components/header.js
+++ ipfs/web/src/components/header.js
@@ -4,7 +4,7 @@ import Link from 'gatsby-link'
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: '#1235f9',
       marginBottom: '1.45rem',
     }}
   >
